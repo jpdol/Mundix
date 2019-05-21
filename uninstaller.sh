b0cee4a4211f2f5aba47fa6c50f311224cc9e92e
João Oliveira <31396276+jvmdoeng16@users.noreply.github.com>
2019-05-21 10:51:47 -0400

    Comandos para desinstalar pacotes Raspbian

---
+++ uninstaller.sh
@@ -0,0 +1,37 @@
+sudo apt-get -y autoremove lxde-common
+sudo apt-get -y autoremove hicolor-icon-theme
+sudo apt-get -y autoremove galculator
+sudo apt-get -y autoremove xarchiver
+sudo apt-get -y autoremove lxde-icon-theme
+sudo apt-get -y autoremove libpoppler19
+sudo apt-get -y autoremove x11-common
+sudo apt-get -y autoremove ed
+sudo apt-get -y autoremove lxsession
+sudo apt-get -y autoremove lxappearance
+sudo apt-get -y autoremove lxpolkit
+sudo apt-get -y autoremove lxrandr
+sudo apt-get -y autoremove lxsession-edit
+sudo apt-get -y autoremove lxshortcut
+sudo apt-get -y autoremove lxtask
+sudo apt-get -y autoremove lxterminal
+sudo apt-get -y autoremove xauth
+sudo apt-get -y autoremove debian-reference-common
+sudo apt-get -y autoremove fontconfig
+sudo apt-get -y autoremove fontconfig-config
+sudo apt-get -y autoremove fonts-freefont-ttf
+sudo apt-get -y autoremove netsurf-gtk
+sudo apt-get -y autoremove netsurf-common
+sudo apt-get -y autoremove dbus-x11
+sudo apt-get -y autoremove desktop-base
+sudo apt-get -y autoremove desktop-file-utils
+sudo apt-get -y autoremove libxmuu1
+sudo rm -rf /opt/vc/src/*
+sudo rm -rf /usr/share/icons/*
+sudo rm -rf /usr/share/sounds/
+sudo rm -rf /usr/share/squeak/
+sudo rm -rf /usr/share/wallpapers
+sudo rm -rf /usr/share/themes
+sudo rm -rf /usr/share/kde4
+sudo rm -rf /usr/share/images/*
+sudo apt-get -y autoremove
+sudo apt-get -y clean
